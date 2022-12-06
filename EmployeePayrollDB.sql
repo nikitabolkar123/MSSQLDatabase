14dc25665f0dcadedea7dd998aaf75062484ddd7
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-12-06 07:54:35 -0800

    Added UC3-InsertValueInTable

--- EmployeePayrollDB.sql
+++ EmployeePayrollDB.sql
@@ -9,3 +9,13 @@ CREATE TABLE EMPLOYEE_PAYROLL
  Salary FLOAT NOT NULL,
  StartDate DATE NOT NULL,
  )
+ 
+  ---UC3----
+ -------Inserting values into Employee_payroll table----------
+
+INSERT INTO employee_payroll VALUES('Nikita','50000','05-Dec-2022');
+INSERT INTO employee_payroll VALUES('Saurabh','40000','06-Dec-2022');
+INSERT INTO employee_payroll VALUES('Priyanshu','15000','08-Dec-2022');
+INSERT INTO employee_payroll VALUES('Rani','10000','12-Jan-2022');
+INSERT INTO employee_payroll VALUES('Anuradha','50000','13-Feb-202');
+
